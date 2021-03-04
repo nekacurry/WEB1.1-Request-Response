c52bacdac83c9b951b10085067770ef1e07fe6f8
nekacurry <anneka498@gmail.com>
2021-03-04 14:44:51 -0600

    finished first 3 routes

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
+from flask import Flask
 
-# TODO: Follow the assignment instructions to complete the required routes!
-# (And make sure to delete this TODO message when you're done!)
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    """Shows a greeting to the user."""
+    return 'Are you there, world? It\'s me, Ducky!'
+
+@app.route('/animal/<users_animal>')
+def favorite_animal(users_animal):
+    """Display a message to the user that changes based on their favorite animal."""
+    return f'Wow, {users_animal} is my favorite animal, too!'
+
+@app.route('/dessert/<users_dessert>')
+def favorite_adessert(users_dessert):
+    """Display a message to the user that changes based on their favorite animal."""
+    return f'How did you know I liked {users_dessert}?'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
